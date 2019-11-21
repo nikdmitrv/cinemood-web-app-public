e3cfff31856e50bcb33b2b07d0ffed2f2c33a332
Julia <tarasova.bmstu@gmail.com>
2019-11-21 21:25:30 +0300

    collections added

--- routes/films.js
+++ routes/films.js
@@ -2,28 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Films = require('../models/films');
 
-// вывод всех фльмов 
+// вывод всех фильмов 
 // allFilms - метод в схеме Films
 router.get('/', async (req, res) => {
-    res.render('./films/index', { allFilms: await Films.allFilms() }, { userLogged })
+    res.render('./films/index', { allFilms: await Films.allFilms(), userLogged })
     //res.send(await Films.allFilms());
-})
-// вывод всех категорий 
-// allCategories - метод в схеме Films
-router.get('/category', async (req, res) => {
-    res.render('./films/category/index', { allCategories: Films.allCategories() }, { userLogged })
 })
 // вывод всех фильмов выбранной категории
 // getByCategory - метод в схеме Films
 router.get('/:category', async (req, res) => {
     res.render('./films/caterory/detail', {
-        categoryFilms: await Films.getByCategory(req.params.category)
-    }, { userLogged })
+        categoryFilms: await Films.getByCategory(req.params.category), userLogged
+    })
 })
 // вывод определённого фильма
 router.get('/:category/:film', async (req, res) => {
     let film = await Films.getByName(req.params.film);
-    res.render('./films/detail', { film }, { userLogged });
+    res.render('./films/detail', { film, userLogged });
 })
 
 module.exports = router;
