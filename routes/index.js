9cf67f66f33f3a7c7d907916116b352b9fb5dc2b
Dmitriy Troitskiy <mitrafantos@gmail.com>
2019-11-22 12:15:46 +0300

    category/detail done

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.redirect('/films');
-})
+  res.redirect('/films');
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,25 +3,31 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
 
+const userLogged = true;
+
 // initial routers
 router.get('/new', (req, res) => {
-    res.render('./users/new');
-})
+  res.render('./users/new');
+});
 router.get('/login', (req, res) => {
-    res.render('./users/login');
-})
+  res.render('./users/login');
+});
 
 // личный кабинет
 // getUser - метод в схеме Users
 router.get('/:id', async (req, res) => {
-    res.render('./users/index', { userData: await User.getById(req.params.id), userLogged });
-})
+  res.render('./users/index', {
+    userData: await User.getById(req.params.id),
+    userLogged,
+  });
+});
 
 // моя коллекция
 router.get('/:id/films', async (req, res) => {
-    let currentUser = await User.getById(req.params.id);
-    res.render('./films/category/detail', {
-        categoryFilms: currentUser.filmsOwned, userLogged
-    })
-})
+  const currentUser = await User.getById(req.params.id);
+  res.render('./films/category/detail', {
+    categoryFilms: currentUser.filmsOwned,
+    userLogged,
+  });
+});
 module.exports = router;
