5c4d91c112bf3dc04d85b25cd1d9e4788ad01265
Julia <tarasova.bmstu@gmail.com>
2019-11-22 09:53:35 +0300

    const userLogged

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
-const userLogged = true;
+
 
 // initial routers
 router.get('/new', (req, res) => {
